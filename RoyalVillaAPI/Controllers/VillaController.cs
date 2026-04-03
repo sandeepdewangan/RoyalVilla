c6fe275d76bb4bf90899e6a3a3194ae67c50576d
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-04-03 20:37:24 +0530

    Route Name, Route Parameter Binding

--- RoyalVillaAPI/Controllers/VillaController.cs
+++ RoyalVillaAPI/Controllers/VillaController.cs
@@ -2,15 +2,40 @@
 
 namespace RoyalVillaAPI.Controllers
 {
+    [Route("/api/villa")]
     [ApiController]
     public class VillaController : ControllerBase
     {
-        // End Point
         [HttpGet]
-        [Route("/villas")]
         public string GetVillas()
         {
             return "Gotted all villas";
         }
+
+        [HttpGet("{id:int}")]
+        public string GetVillas(int id)
+        {
+            return "Got villa no. " + id;
+        }
+
+        // Here name is by default string type, no need to define
+        // By default the parameters are [FromRequest]
+        // https://localhost:7138/api/villa/{id}/{name}
+
+
+        [HttpGet("{id:int}/{name}")]
+        public string GetVillas(int id, string name)
+        {
+            return "Got villa no. " + id + name;
+        }
+
+        // It will get parameters from query parameters
+        // https://localhost:7138/api/villa?id=2&name=sandeep
+        [HttpGet]
+        public string GetVillasFromQuery([FromQuery] int id, [FromQuery] string name)
+        {
+            return "Got villa no. " + id + name;
+        }
+        // Similarly we can have parameters from header using [FromHeader]
     }
 }
