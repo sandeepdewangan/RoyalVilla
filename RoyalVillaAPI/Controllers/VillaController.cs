2eb87e7527d2a189746ee2fc24563fe3730fa77b
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-04-03 20:38:38 +0530

    -

--- RoyalVillaAPI/Controllers/VillaController.cs
+++ RoyalVillaAPI/Controllers/VillaController.cs
@@ -13,29 +13,10 @@ namespace RoyalVillaAPI.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public string GetVillas(int id)
+        public string GetVillasById(int id)
         {
             return "Got villa no. " + id;
         }
 
-        // Here name is by default string type, no need to define
-        // By default the parameters are [FromRequest]
-        // https://localhost:7138/api/villa/{id}/{name}
-
-
-        [HttpGet("{id:int}/{name}")]
-        public string GetVillas(int id, string name)
-        {
-            return "Got villa no. " + id + name;
-        }
-
-        // It will get parameters from query parameters
-        // https://localhost:7138/api/villa?id=2&name=sandeep
-        [HttpGet]
-        public string GetVillasFromQuery([FromQuery] int id, [FromQuery] string name)
-        {
-            return "Got villa no. " + id + name;
-        }
-        // Similarly we can have parameters from header using [FromHeader]
     }
 }
