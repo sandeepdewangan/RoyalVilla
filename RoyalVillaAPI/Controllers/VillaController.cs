c6a0845ed17575a94b3f153c8fd43077346a0044
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-04-03 20:10:19 +0530

    Basic API Controller with a End Point

---
+++ RoyalVillaAPI/Controllers/VillaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoyalVillaAPI.Controllers
+{
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        // End Point
+        [HttpGet]
+        [Route("/villas")]
+        public string GetVillas()
+        {
+            return "Gotted all villas";
+        }
+    }
+}
